69fa4313b630b872b930794d79bccc9020d266c2
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-06 19:16:59 +0530

    Day6

--- Counter/app.js
+++ Counter/app.js
@@ -29,7 +29,7 @@ btns.forEach(function (btn) {
         if (count < 0) {
             value.style.color = "red";
         }
-        if (count === 0) {
+        if (count == 0) {
             value.style.color = "#222";
         }
 
